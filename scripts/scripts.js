0790803d9453ac134d96f1fe614240eb57fe2193
Kitanna <Karl-Kitanna@Admins-MacBook-Pro.local>
2025-05-05 09:12:12 +0300

    Add an event-listener to the scripts file add 'click' event listener which is not actually working yet

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+const startButton = document.getElementById('button')
+
+startButton.addEventListener('click', startWeb)
+
+function startWeb() {
+    console.log('Web Started')
+}
+
+
+console.log('Web Started 2')
